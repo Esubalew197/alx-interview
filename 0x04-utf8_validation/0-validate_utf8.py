31afd0c6f9d9dbff298fa54945e42c2b20dd26a6
Esubalew197 <esubalewmillion@gmail.com>
2024-01-05 00:10:02 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python3
-def validUTF8(data):
-    # Variable to track the number of bytes in the current UTF-8 character
-    num_bytes = 0
+'''UTF-* validation module
+'''
 
-    for byte in data:
-        # Check if the current byte is the start of a new UTF-8 character
-        if num_bytes == 0:
-            if (byte >> 7) == 0b0:
-                # Single-byte character
-                num_bytes = 0
-            elif (byte >> 5) == 0b110:
-                # Two-byte character
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                # Three-byte character
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                # Four-byte character
-                num_bytes = 3
+
+def validUTF8(data):
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
             else:
-                # Invalid start of a character
+                return False
+        elif data[i] & 0b11110000 == 0b11100000:
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
                 return False
         else:
-            # Check if the current byte is a continuation byte
-            if (byte >> 6) == 0b10:
-                num_bytes -= 1
-            else:
-                # Invalid continuation byte
-                return False
-
-    # Check if all characters are complete (num_bytes is back to 0)
-    return num_bytes == 0
+            return False
+    return True
